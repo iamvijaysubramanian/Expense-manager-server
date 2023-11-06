c7d6c526fd959bd918549555abfc7456b843b3fd
vijay subramanian <vijaysubbu69@gmail.com>
2023-11-06 12:03:09 +0530

    bug fixed

--- models/userModel.js
+++ models/userModel.js
@@ -1,29 +1,25 @@
 const mongoose = require("mongoose");
 
+//schema design
 const userSchema = new mongoose.Schema(
   {
-    username: {
+    name: {
       type: String,
-      required: true,
-      unique: true,
-      trim: true,
-      minlength: 3,
-      maxlength: 20,
+      required: [true, "name is required"],
     },
     email: {
       type: String,
-      required: true,
+      required: [true, "email is required and should be unique"],
       unique: true,
-      trim: true,
     },
     password: {
       type: String,
-      required: true,
-      minlength: 6, // You can adjust the minimum password length
+      required: [true, "password is required"],
     },
   },
   { timestamps: true }
 );
 
+//export
 const userModel = mongoose.model("users", userSchema);
 module.exports = userModel;
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 const dotenv = require("dotenv");
+const colors = require("colors");
 const connectDb = require("./config/connectDb");
-
 // config dot env file
 dotenv.config();
 
@@ -13,11 +14,13 @@ connectDb();
 const app = express();
 
 //middlewares
+// app.use(morgan("dev"));
 app.use(express.json());
 app.use(cors("*"));
 
 //routes
-app.use("api/v1/users", require("./routes/userRoute"));
+//user routes
+app.use("/api/v1/users", require("./routes/userRoute"));
 //transections routes
 app.use("/api/v1/transactions", require("./routes/transactionRoutes"));
 
