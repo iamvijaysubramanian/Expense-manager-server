9745e835448796c54f3edd5c39908405972de43c
vijay subramanian <vijaysubbu69@gmail.com>
2023-11-06 12:46:16 +0530

    password validated

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 //schema design
 const userSchema = new mongoose.Schema(
@@ -6,15 +7,31 @@ const userSchema = new mongoose.Schema(
     name: {
       type: String,
       required: [true, "name is required"],
+      unique: true,
     },
     email: {
       type: String,
       required: [true, "email is required and should be unique"],
       unique: true,
+      validate: {
+        validator: (value) => {
+          return validator.isEmail(value);
+        },
+        message: "Invalid email address",
+      },
     },
     password: {
       type: String,
       required: [true, "password is required"],
+      validate: {
+        validator: (value) => {
+          const passwordRegex =
+            /^(?=.*[A-Z])(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
+          return passwordRegex.test(value);
+        },
+        message:
+          "Password must be at least 8 characters long and contain at least 1 uppercase letter and 1 special character.",
+      },
     },
   },
   { timestamps: true }
